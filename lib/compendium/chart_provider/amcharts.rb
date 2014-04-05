df2e2050f194c223d6f439b7adbb527ec5bff793
Daniel Vandersluis <dvandersluis@selfmgmt.com>
2014-04-05 17:28:20 -0400

    Allow for remote data loading

--- lib/compendium/chart_provider/amcharts.rb
+++ lib/compendium/chart_provider/amcharts.rb
@@ -4,8 +4,19 @@ module Compendium
   module ChartProvider
     # Uses the amcharts.rb gem to provide charting
     class AmCharts < Compendium::AbstractChartProvider
-      def initialize(type, data, &setup_proc)
-        @chart = chart_class(type).new(data, &setup_proc)
+      def initialize(type, data_or_url, params = {}, &setup_proc)
+        if data_or_url.is_a?(String)
+          url = data_or_url
+
+          @chart = chart_class(type).new([]) do |c|
+            c.data_source = { url: url, params: params || {}, method: 'POST' }
+          end
+
+          @chart.update_settings(&setup_proc)
+        else
+          data = data_or_url
+          @chart = chart_class(type).new(data, &setup_proc)
+        end
       end
 
       def render(template, container)
